99198ffd8ade92d8322d022cb48a1178eae8ba87
rafaelEstevam <rafael.estevamdesign@gmail.com>
2024-04-23 15:59:01 -0300

    modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-modal',
+  selector: 'modal-component',
   standalone: true,
   imports: [],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
 export class ModalComponent {
+  // @Input() showModal:boolean = false;
+  // @Input() modalName: string = '';
 
+  // @Output() closeModal = new EventEmitter<any>();
+
+  // emitEvent() {
+  //   this.closeModal.emit('');
+  // }
+
+  // openModal(){
+  //   this.showModal = true;
+  // };
+
+  // closeModal(){
+  //   this.showModal = false;
+  // }
+
+  // onMounted(){
+  //   console.log('teste');
+  //   console.log(this.modalName)
+  // }
 }
--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'home-component',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [CommonModule, HeaderComponent, ModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  // showModal: boolean = false;
+
+  handleShowModal(modal:string){
+    console.log(modal);
+  }
+
+  // handleHideModal(){
+  //   console.log('hide');
+  //   this.showModal = false;
+  // }
 
 }
