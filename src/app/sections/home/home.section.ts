f7b4eff6ef32c00c49938913bd13b582d7979f13
Rafael Estevam de Oliveira <rafael.estevamdesign@gmail.com>
2024-05-08 17:37:01 +0000

    remove comments

--- src/app/sections/home/home.section.ts
+++ src/app/sections/home/home.section.ts
@@ -38,19 +38,6 @@ export class HomeSection {
   error: any;
   loading : boolean = false;
 
-  // public carreer: Curriculum[] = [
-  //   // {id: '2', companyName: 'Printi', shortDescription: 'Suporte e manutenção de e-commerce com ReactJS, Next e etc.', startDate: '06-2023', name: 'Desenvolvedor Front-end', endDate: ''},
-  //   // {id: '3', companyName: 'FITec', shortDescription: 'Desenvolvimento de front-end para aplicação de CRM com ReactJS, Next e etc.', startDate: '02-2021', name: 'Desenvolvedor Front-end', endDate: '06-2023'},
-  //   // {id: '4', companyName: 'Gauge', shortDescription: 'Desenvolvimento de front-end para aplicação de gestão de manutenção de aeronaves com ReactJS.', startDate: '02-2019', name: 'Desenvolvedor Front-end', endDate: '02-2021'},
-  //   // {id: '5', companyName: 'Univalle', shortDescription: 'Desenvolvimento de front-end para aplicação de emissão de nota fiscal com AngularJS.', startDate: '01-2017', name: 'Desenvolvedor Front-end', endDate: '02-2019'},
-  //   // {id: '6', companyName: 'Estevam Design', shortDescription: 'Desenvolvimento fullstack de projetos feitos como freelancer, usando desde Wordpress a React e Next.', startDate: '06-2015', name: 'Desenvolvedor Fullstack', endDate: ''},
-  // ];
-
-  // public graduations: Curriculum[] = [];
-  // public companies: Curriculum[] = [];
-  // public tecnologies: any = [];
-  // public mainTecnologies: any = [];
-
   handleOpenModal({id, type}: any){
     this.showModal.set(true);
     if(type === 'company'){
